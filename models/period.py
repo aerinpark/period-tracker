18c52b5b204fdc955efb6005738363032b0dbc98
aerinp <78767681+aerinpark@users.noreply.github.com>
2026-07-25 00:02:29 -0700

    Implement Period day management

--- models/period.py
+++ models/period.py
@@ -1,4 +1,5 @@
 import datetime
+from period_day import PeriodDay
 
 class Period:
     """
@@ -13,4 +14,37 @@ class Period:
         self.start_date = start_date
         self.end_date = end_date
         self.days = []
+
+    def duration(self):
+        """
+        Returns the duration of the period in days.
+
+        The duration is inclusive of both the start and end dates.
+        Returns None if the period has ot ended.
+        """
+        return (self.end_date - self.start_date).days + 1
+
+    def _update_date_range(self, new_date: datetime):
+        """
+        Updates the period's date range to include the given date.
+
+        Intended for internal use when adding or modifying daily period records.
+        """
+        if self.start_date > new_date.date:
+            self.start_date = new_date.date
+
+        if self.end_date < new_date.date:
+            self.end_date = new_date.date
+
+    def add_day(self, day: PeriodDay):
+        """
+        Adds a daily record to the period cycle.
+
+        Updates the period's date range if necessary.
+        """
+        self._update_date_range(day.date)
+        self.days.append(day)
+
+
+
 
